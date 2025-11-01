d8a9cc70d617c59be7018e7b4caa6784c43984c5
reshadMajumder <reshad22205341197@diu.edu.bd>
2025-11-01 10:08:31 +0600

    added  whitenoise in wsgi

--- core/core/wsgi.py
+++ core/core/wsgi.py
@@ -10,8 +10,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+# Wrap with WhiteNoise so static files in STATIC_ROOT are served when DEBUG=False
+# This is useful for deployments (e.g., Vercel) where a separate static file server isn't configured.
+if not settings.DEBUG:
+	application = WhiteNoise(application, root=str(settings.STATIC_ROOT), prefix='static/')
+
 app = application
